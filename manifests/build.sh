5c588e0d0cee306af2fa639fe12e78d07afbf421
Chris Arnold <carnold@cs.vt.edu>
2019-03-26 13:56:28 -0400

    fix nginx manifests

--- manifests/build.sh
+++ manifests/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-cat namespace.yml config-haproxy.yml rbac.yml watcher.yml actor-haproxy.yml > k8slb-haproxy.yml
-cat namespace.yml config-nginx.yml rbac.yml watcher.yml actor-nginx.yml > k8slb-nginx.yml
-cat namespace.yml config-kubeproxy.yml rbac.yml watcher.yml actor-kubeproxy.yml > k8slb-kubeproxy.yml
+cat namespace.yml config-haproxy.yml rbac.yml watcher-haproxy.yml actor-haproxy.yml > k8slb-haproxy.yml
+cat namespace.yml config-nginx.yml rbac.yml watcher-nginx.yml actor-nginx.yml > k8slb-nginx.yml
+cat namespace.yml config-kubeproxy.yml rbac.yml watcher-kubeproxy.yml actor-kubeproxy.yml > k8slb-kubeproxy.yml
