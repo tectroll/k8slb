967c971755b59d2153f30065e191459a5a2cb9fb
carnold <carnold@cs.vt.edu>
2019-04-02 13:38:08 -0400

    release

--- manifests/build.sh
+++ manifests/build.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
-cat namespace.yml config-haproxy.yml rbac.yml watcher-haproxy.yml actor-haproxy.yml > k8slb-haproxy.yml
-cat namespace.yml config-nginx.yml rbac.yml watcher-nginx.yml actor-nginx.yml > k8slb-nginx.yml
-cat namespace.yml config-kubeproxy.yml rbac.yml watcher-kubeproxy.yml actor-kubeproxy.yml > k8slb-kubeproxy.yml
+source ../VERSION
+
+cat namespace.yml config-haproxy.yml rbac.yml > k8slb-haproxy.yml
+sed "s/:latest/:$VERSION/" watcher-haproxy.yml >> k8slb-haproxy.yml
+sed "s/:latest/:$VERSION/" actor-haproxy.yml >> k8slb-haproxy.yml
+
+cat namespace.yml config-nginx.yml rbac.yml > k8slb-nginx.yml
+sed "s/:latest/:$VERSION/" watcher-nginx.yml >> k8slb-nginx.yml
+sed "s/:latest/:$VERSION/" actor-nginx.yml >> k8slb-nginx.yml
+
+cat namespace.yml config-kubeproxy.yml rbac.yml > k8slb-kubeproxy.yml
+sed "s/:latest/:$VERSION/" watcher-kubeproxy.yml >> k8slb-kubeproxy.yml
+sed "s/:latest/:$VERSION/" actor-kubeproxy.yml >> k8slb-kubeproxy.yml
+
